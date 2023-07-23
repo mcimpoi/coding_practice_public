25f3ca793f4632916ef414dd6bc602fc7078703b
Mircea C <1849908+mcimpoi@users.noreply.github.com>
2023-07-23 18:09:01 +0200

    Add daily - fast exp.

---
+++ leetcode/cpp/P0050_Pow.cpp
@@ -0,0 +1,42 @@
+class Solution {
+ public:
+  double myPow(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (x == 0) {
+      return 0;
+    }
+    if (x == 1) {
+      return 1;
+    }
+
+    if (x == -1) {
+      return n % 2 == 0 ? 1.0 : -1.0;
+    }
+
+    long long n_ = static_cast<long long>(n);
+    bool div = false;
+    if (n_ < 0) {
+      div = true;
+      n_ = -n_;
+    }
+
+    double result = 1;
+
+    while (n_ > 0) {
+      while (n_ > 0 && 0 == n_ % 2) {
+        n_ /= 2;
+        x *= x;
+      }
+
+      --n_;
+      result *= x;
+    }
+
+    if (div) {
+      return 1. / result;
+    }
+    return result;
+  }
+};
